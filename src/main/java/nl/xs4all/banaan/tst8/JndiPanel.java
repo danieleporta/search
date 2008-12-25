060a2f1558a02c63d3612050bb82b629aef670d5
Erik van Konijnenburg <ekonijn@xs4all.nl>
2008-12-25 15:00:17 +0100

    First working jndi access
    src/main/java/nl/xs4all/banaan/tst8/JndiPanel.html
     * provide binding list
    src/main/java/nl/xs4all/banaan/tst8/JndiPanel.java
     * extract jndi bindings at java:comp/env
     * show these in propertylistview
    src/main/webapp/WEB-INF/web.xml
     * add meaningless settings to be made visible via jndi

--- src/main/java/nl/xs4all/banaan/tst8/JndiPanel.java
+++ src/main/java/nl/xs4all/banaan/tst8/JndiPanel.java
@@ -1,11 +1,19 @@
 package nl.xs4all.banaan.tst8;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.naming.Binding;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 
 import org.apache.log4j.Logger;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.markup.html.panel.Panel;
 
 /**
@@ -30,8 +38,44 @@ public class JndiPanel extends Panel {
             logger.error("Naming exception");
             getSession().error("naming exception");
         }
-        add (new Label("initial", ic));
+        add(new Label("initial", ic));
+
+        // JNDI can query multiple namespaces;
+        // the java:comp/env path gives access to bindings
+        // that are provided by web.xml or the container.  
+        // See servlet 2.4 spec, SRV 9.11, SRV 13.4.20, SRV 13.4.24, via
+        // http://jcp.org/aboutJava/communityprocess/final/jsr154/index.html
+        // 
+        String path = "java:comp/env/";
+        add(new Label("path", path));
         
-        
+        add(
+                new PropertyListView("bindings", jndiBindingList(path)) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void populateItem (ListItem item) {
+                item.add(new Label("name"));
+                item.add(new Label("object"));
+            }
+        });
+
+    }
+
+    private List<Binding> jndiBindingList(String name) {
+        List<Binding> result = new LinkedList<Binding>();
+        try {
+            NamingEnumeration<Binding> e = initialContext.listBindings(name);
+            while (e.hasMoreElements()) {
+                logger.info("adding element");
+                result.add((Binding) e.nextElement());
+            }            
+        }
+        catch (NamingException ne) {
+            logger.error("Naming exception in listbindings");
+            getSession().error("naming exception in listbindings");     
+        }
+
+        return Collections.unmodifiableList(result);
     }
 }
