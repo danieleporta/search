20db7c44b89a79f06308e999041b6d890c5fafcb
Erik van Konijnenburg <ekonijn@xs4all.nl>
2008-12-25 11:33:56 +0100

    Use PropertyListView to simplify code.
    src/main/java/nl/xs4all/banaan/tst8/PropertyPanel.html
     * small title change to verify deployment
    src/main/java/nl/xs4all/banaan/tst8/PropertyPanel.java
     * dont roll your own getListItemModel,
       just use a PropertyListView that makes the compoundpropertymodel
       for you

--- src/main/java/nl/xs4all/banaan/tst8/PropertyPanel.java
+++ src/main/java/nl/xs4all/banaan/tst8/PropertyPanel.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.IModel;
 
 public class PropertyPanel extends Panel {
     List<Binding> props;
@@ -15,15 +13,9 @@ public class PropertyPanel extends Panel {
     public PropertyPanel(String id, List<Binding> props) {
         super(id);
         this.props = props;
-        add (new ListView ("props", this.props) {
+        add (new PropertyListView ("props", this.props) {
             private static final long serialVersionUID = 1L;
-            
-            @Override
-            protected IModel getListItemModel(final IModel listViewModel, final int index)
-            {
-                return new CompoundPropertyModel(super.getListItemModel(listViewModel, index));
-            }
-            
+           
             @Override
             public void populateItem (ListItem item) {
                 item.add(new Label("key"));
