d6137b63de77c19a9e487f3f10bc4feb48398342
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-01-11 12:47:09 +0100

    New test for clickability of links provided on basepage

---
+++ src/test/java/nl/xs4all/banaan/tst8/web/base/BasePageTest.java
@@ -0,0 +1,58 @@
+package nl.xs4all.banaan.tst8.web.base;
+
+
+import nl.xs4all.banaan.tst8.fixtures.Fixtures;
+import nl.xs4all.banaan.tst8.web.home.HomePage;
+import nl.xs4all.banaan.tst8.web.jndi.JndiPage;
+import nl.xs4all.banaan.tst8.web.param.ParamPage;
+import nl.xs4all.banaan.tst8.web.property.PropertyPage;
+
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Verify that the links provided by the base page are clickable
+ * and lead to expected type of destination.
+ * @author konijn
+ *
+ */
+public class BasePageTest {
+    private Fixtures fixtures;
+    private WicketTester tester;
+
+    @Before
+    public void setUp() {
+        fixtures = new Fixtures();
+        tester = fixtures.getTester();
+    }
+
+    @Test
+    public void testClickLink1() {
+        tester.startPage(HomePage.class);
+        // tester.debugComponentTrees();
+        tester.clickLink("menu:bindings:0:value");
+        fixtures.checkBasePage(HomePage.class);
+    }
+    
+    @Test
+    public void testClickLink2() {
+        tester.startPage(HomePage.class);
+        tester.clickLink("menu:bindings:1:value");
+        fixtures.checkBasePage(JndiPage.class);
+    }
+    
+    @Test
+    public void testClickLink3() {
+        tester.startPage(HomePage.class);
+        tester.clickLink("menu:bindings:2:value");
+        fixtures.checkBasePage(ParamPage.class);
+    }
+    
+    @Test
+    public void testClickLink4() {
+        tester.startPage(HomePage.class);
+        tester.clickLink("menu:bindings:3:value");
+        fixtures.checkBasePage(PropertyPage.class);
+    }
+}
