27f668bc4b832871eacfd15aab9f41786ac21ea0
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-01-11 12:10:21 +0100

    drop redundant import

--- src/main/java/nl/xs4all/banaan/tst8/web/base/BasePage.java
+++ src/main/java/nl/xs4all/banaan/tst8/web/base/BasePage.java
@@ -2,7 +2,6 @@ package nl.xs4all.banaan.tst8.web.base;
 
 import nl.xs4all.banaan.tst8.web.menu.MenuPanel;
 
-import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 
