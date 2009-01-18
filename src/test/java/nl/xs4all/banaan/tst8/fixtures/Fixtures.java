e02b8935e22fa0be82357a788807873d10e9a9df
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-01-18 02:26:24 +0100

    Implement all test fixtures in terms of springified components

--- src/test/java/nl/xs4all/banaan/tst8/fixtures/Fixtures.java
+++ src/test/java/nl/xs4all/banaan/tst8/fixtures/Fixtures.java
@@ -7,13 +7,11 @@ import static org.junit.matchers.JUnitMatchers.containsString;
 import java.lang.reflect.InvocationTargetException;
 
 import javax.naming.Context;
-import javax.naming.NamingException;
 
 import nl.xs4all.banaan.tst8.service.JndiReader;
 import nl.xs4all.banaan.tst8.service.JndiReaderImpl;
-import nl.xs4all.banaan.tst8.service.NotificatorImpl;
+import nl.xs4all.banaan.tst8.service.Notificator;
 import nl.xs4all.banaan.tst8.service.PropertyReader;
-import nl.xs4all.banaan.tst8.service.PropertyReaderImpl;
 import nl.xs4all.banaan.tst8.service.ServiceException;
 import nl.xs4all.banaan.tst8.web.DemoApplication;
 import nl.xs4all.banaan.tst8.web.base.BasePage;
@@ -25,7 +23,6 @@ import org.apache.wicket.util.tester.ITestPageSource;
 import org.apache.wicket.util.tester.WicketTester;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.mock.jndi.SimpleNamingContext;
 
 /**
  * Various convenience objects for use during unit testing.
@@ -34,63 +31,47 @@ import org.springframework.mock.jndi.SimpleNamingContext;
  */
 public class Fixtures {
     private ApplicationContext ctx;
-    private Context initialContext;
-    private JndiReaderImpl jndiReader;
-    private PropertyReader propertyReader;
-    private MailSenderFixture mailSenderFixture;
-    private DemoApplication application;
     private WicketTester tester;
 
 
     public Fixtures () {
         ctx = new ClassPathXmlApplicationContext("testContext.xml");
-        initialContext = (Context) ctx.getBean("jndiRootContext", Context.class);
-        jndiReader = (JndiReaderImpl) ctx.getBean("jndiReader", JndiReaderImpl.class);
-        propertyReader = (PropertyReader) ctx.getBean("propertyReader", PropertyReaderImpl.class);
+        tester = (WicketTester) ctx.getBean("tester", WicketTester.class);
     }
     
     public Context getInitialContext() {
-        return initialContext;
+        return (Context) ctx.getBean("jndiRootContext", Context.class);
     }
 
     public JndiReader getJndiReader() {
-        return jndiReader;
+        return (JndiReaderImpl) ctx.getBean("jndiReader", JndiReaderImpl.class);
     }
 
     public MailSenderFixture getMailSenderFixture() {
-        if (mailSenderFixture == null) {
-            mailSenderFixture = new MailSenderFixture();
-        }
-        return mailSenderFixture;
+        return (MailSenderFixture) ctx.getBean("mailSender", MailSenderFixture.class);
+    }
+
+    public PropertyReader getPropertyReader() {
+        return (PropertyReader) ctx.getBean("propertyReader", PropertyReader.class);
+    }
+    
+    public Notificator getNotificator() {
+        return (Notificator) ctx.getBean("notificator", Notificator.class);
     }
 
     public DemoApplication getApplication() {
-        if (application == null) {
-            application = new DemoApplication();
-            
-            propertyReader = new PropertyReaderImpl();
-            application.setPropertyReader(propertyReader);
-            application.setJndiReader(getJndiReader());
-            
-            NotificatorImpl notificator = new NotificatorImpl();
-            notificator.setMailSender(getMailSenderFixture());
-            application.setNotificator(notificator);
-        }
-        return application;
+         return (DemoApplication) ctx.getBean("demoApplication", DemoApplication.class);
     }
 
     public WicketTester getTester() {
-        if (tester == null) {
-            tester = new WicketTester(getApplication());
-        }
         return tester;
     }
 
+    
     /**
      * check that a rendered subclass of basepage has expected class 
      * and contents
      */
-    
     public void checkBasePage(Class<? extends BasePage> pageClass, String ... patterns) {
         tester.assertRenderedPage(pageClass);
         
@@ -138,6 +119,5 @@ public class Fixtures {
         }
         
     }
-
 
 }
