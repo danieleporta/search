9d50252270fe4c747041c8e5f110ca4ef056e05a
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-01-25 09:58:08 +0100

    Avoid serialisation problem in JndiPanel
    By retrieving the list in getObject rather than in constructor.

--- src/main/java/nl/xs4all/banaan/tst8/web/jndi/JndiPanel.java
+++ src/main/java/nl/xs4all/banaan/tst8/web/jndi/JndiPanel.java
@@ -5,12 +5,14 @@ import nl.xs4all.banaan.tst8.service.ServiceException;
 import nl.xs4all.banaan.tst8.util.GenericBinding;
 import nl.xs4all.banaan.tst8.web.DemoApplication;
 
+import org.apache.log4j.Logger;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 
 /**
  * Show JNDI data, given a location to find them
@@ -18,14 +20,28 @@ import org.apache.wicket.markup.html.panel.Panel;
  */
 public class JndiPanel extends Panel {
     private static final long serialVersionUID = 1L;
+    private static final Logger logger = Logger.getLogger(JndiPanel.class);
 
     public JndiPanel(String id, final String location) throws ServiceException {
         super(id);
+        getSession().info("building jndi panel");
 
-        JndiList jndiList = DemoApplication.get().
-                getServices().getJndiReader().read(location);
+        // Make model dynamic to avoid it being serialised into the session.
+        Model model = new Model() {
+            @Override
+            public Object getObject() {
+                try {
+                    return DemoApplication.get().getServices().
+                        getJndiReader().read(location).getList();
+                } catch (ServiceException se) {
+                    logger.error("Caught Service Exception", se);
+                    throw new RuntimeException(se);
+                }
+            }
+        };
+        
         add(new Label("location", location));
-        add(new PropertyListView("bindings", jndiList.getList()) {
+        add(new PropertyListView("bindings", model) {
             private static final long serialVersionUID = 1L;
 
             @Override
