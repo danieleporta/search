5be9aae8395d40155621a760e4fdb95cad568400
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-02-21 16:15:55 +0100

    bugfix addressPanel
    src/main/java/nl/xs4all/banaan/tst8/web/address/AddressPanel.java
     * add compoundpropertymodel to actually access the properties
    
    src/test/java/nl/xs4all/banaan/tst8/web/address/AddressPanelTest.java
     * assert addres contents is shown
     * modify model to avoid serialisation exceptions

--- src/main/java/nl/xs4all/banaan/tst8/web/address/AddressPanel.java
+++ src/main/java/nl/xs4all/banaan/tst8/web/address/AddressPanel.java
@@ -2,6 +2,7 @@ package nl.xs4all.banaan.tst8.web.address;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 
 /**
@@ -12,8 +13,9 @@ import org.apache.wicket.model.IModel;
 public class AddressPanel extends Panel {
     private static final long serialVersionUID = -8781531432652666470L;
 
+    /** model must return an address as object */
     public AddressPanel(String id, IModel model) {
-        super(id, model);
+        super(id, new CompoundPropertyModel(model));
         add (new Label("name"));
         add (new Label("street"));
         add (new Label("city"));
--- src/test/java/nl/xs4all/banaan/tst8/web/address/AddressPanelTest.java
+++ src/test/java/nl/xs4all/banaan/tst8/web/address/AddressPanelTest.java
@@ -1,12 +1,17 @@
 package nl.xs4all.banaan.tst8.web.address;
 
-import static nl.xs4all.banaan.tst8.fixtures.DomainObjects.*;
+import static nl.xs4all.banaan.tst8.fixtures.DomainObjects.ADDRESS1;
+import static nl.xs4all.banaan.tst8.fixtures.DomainObjects.CITY1;
+import static nl.xs4all.banaan.tst8.fixtures.DomainObjects.NAME1;
+import static nl.xs4all.banaan.tst8.fixtures.DomainObjects.STREET1;
+
 import javax.annotation.Resource;
 
 import nl.xs4all.banaan.tst8.fixtures.BasePageTester;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.util.tester.TestPanelSource;
@@ -42,19 +47,29 @@ public class AddressPanelTest {
             private static final long serialVersionUID = 1L;
 
             public Panel getTestPanel(String panelId) {
-                return new AddressPanel(panelId, new LoadableDetachableModel() {
-                    private static final long serialVersionUID = 1L;
-
-                    @Override
-                    protected Object load() {
-                        return ADDRESS1;
-                    }
-                });
+                return new AddressPanel(panelId, new AddressModel());
             }
         });
         tester.assertComponent("panel:name", Label.class);
         tester.assertComponent("panel:street", Label.class);
         tester.assertComponent("panel:city", Label.class);
+        tester.assertContains(NAME1);
+        tester.assertContains(STREET1);
+        tester.assertContains(CITY1);
+    }
+    
+    private static class AddressModel implements IModel {
+        private static final long serialVersionUID = 1L;
+
+        public Object getObject() {
+            return ADDRESS1;
+        }
+
+        public void setObject(Object object) {
+        }
+
+        public void detach() {
+        }
     }
 
 }
