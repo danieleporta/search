68aeab45d4d7938f1c19a4ed70f4a7b82f74fa0e
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-02-27 12:37:17 +0100

    BuildInfo viaspring wiring
    PLace a buildInfo object in the Spring test context.
    
    src/test/java/nl/xs4all/banaan/tst8/service/BuildInfoImplTest.java
     * use spring framework
     * test content of wired in object
    
    src/test/resources/dummy-build.properties
     * dummy content for testing.
    
    src/test/resources/testContext.xml b/src/test/resources/testContext.xml
     * use util namespace to read property file
     * wire in the buildInfo object

--- src/test/java/nl/xs4all/banaan/tst8/service/BuildInfoImplTest.java
+++ src/test/java/nl/xs4all/banaan/tst8/service/BuildInfoImplTest.java
@@ -3,9 +3,19 @@ package nl.xs4all.banaan.tst8.service;
 import static org.junit.Assert.assertEquals;
 import static nl.xs4all.banaan.tst8.fixtures.DomainObjects.*;
 
-import org.junit.Test;
+import javax.annotation.Resource;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/testContext.xml"})
 public class BuildInfoImplTest {
+    
+    @Resource
+    private BuildInfo buildInfo;
    
     @Test
     public void testBuildInfoFieldsExist() {
@@ -14,5 +24,13 @@ public class BuildInfoImplTest {
         assertEquals(BUILD_VERSION1, BUILD_INFO1.getVersion());        
         assertEquals(BUILD_USER1, BUILD_INFO1.getUser());
     }
+    
+    @Test
+    public void testBuildInfoFieldsExistForSpringWiredBean() {
+        assertEquals(BUILD_NAME1, buildInfo.getName());
+        assertEquals(BUILD_GROUP1, buildInfo.getGroup());
+        assertEquals(BUILD_VERSION1, buildInfo.getVersion());        
+        assertEquals(BUILD_USER1, buildInfo.getUser());
+    }    
 
 }
