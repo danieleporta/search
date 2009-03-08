b4ff6f8c3486fe82e86536152e73dbae44b14afd
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-03-08 11:03:25 +0100

    Exercise onSubmti() of the test form

--- src/main/java/nl/xs4all/banaan/tst8/web/form/FormPanel.java
+++ src/main/java/nl/xs4all/banaan/tst8/web/form/FormPanel.java
@@ -1,14 +1,29 @@
 package nl.xs4all.banaan.tst8.web.form;
 
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.value.ValueMap;
+
 
 public class FormPanel extends Panel {
     private static final long serialVersionUID = -190918054405562036L;
     
     public FormPanel(String id, IModel model) {
         super(id, model);
-        add(new TextField("text"));
+        Form form = new Form("form", model) {
+            private static final long serialVersionUID = -5412242628570636316L;
+
+            @Override
+            protected void onSubmit() {
+                ValueMap map = (ValueMap) getModelObject();
+                map.put("submitSeen", "true");
+            }
+        };
+        add(form);
+        form.add(new TextField("text"));
+        form.add(new Label("submitSeen"));
     }
 }
--- src/test/java/nl/xs4all/banaan/tst8/web/form/FormPanelTest.java
+++ src/test/java/nl/xs4all/banaan/tst8/web/form/FormPanelTest.java
@@ -6,6 +6,7 @@ import nl.xs4all.banaan.tst8.fixtures.BasePageTester;
 
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.util.tester.FormTester;
 import org.apache.wicket.util.tester.TestPanelSource;
 import org.apache.wicket.util.value.ValueMap;
 import org.junit.Test;
@@ -29,7 +30,7 @@ public class FormPanelTest {
     
     /** show a form, don't push any buttons */
     @Test
-    public void testAddressPanel() {
+    public void testPanelShowing() {
         tester.startPanel(new TestPanelSource() {
             private static final long serialVersionUID = 1L;
 
@@ -39,7 +40,27 @@ public class FormPanelTest {
                                 new ValueMap("text=required,submitSeen=false")));
             }
         });
-        tester.assertModelValue("panel:text", "required");
+        tester.assertModelValue("panel:form:text", "required");
     }
 
+    /** standard submit sequence */
+    @Test
+    public void testFormPanelSubmit() {
+        tester.startPanel(new TestPanelSource() {
+            private static final long serialVersionUID = 1L;
+
+            public Panel getTestPanel(String panelId) {
+                return new FormPanel(panelId,
+                        new CompoundPropertyModel(
+                                new ValueMap("text=required,submitSeen=false")));
+            }
+        });
+
+        tester.assertModelValue("panel:form:submitSeen", "false");
+        FormTester formTester = tester.newFormTester("panel:form");
+        formTester.submit();
+        tester.assertModelValue("panel:form:submitSeen", "true");
+    }
+    
+    
 }
