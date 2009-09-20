1578d18ab70466ea9f6973f8bfd53fe929032460
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-09-20 16:31:02 +0200

    testing upload in combination with text fields
     * in uploadPanelTest, drop broken use of spring-injected tester.
       still needs to be fixed elsewhere.
     * find a way to make formTester behave like a browser that does
       not upload a file.

--- src/test/java/nl/xs4all/banaan/tst8/web/upload/UploadPanelTest.java
+++ src/test/java/nl/xs4all/banaan/tst8/web/upload/UploadPanelTest.java
@@ -11,6 +11,7 @@ import java.net.URL;
 import javax.annotation.Resource;
 
 import nl.xs4all.banaan.tst8.fixtures.BasePageTester;
+import nl.xs4all.banaan.tst8.web.DemoApplication;
 
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
@@ -34,12 +35,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class UploadPanelTest {
 
     @Resource
+    private DemoApplication demoApplication;
+    
     private BasePageTester tester;
-    private ValueMap map;
+    private ValueMap map;                   // model for the form
 
     @Before
     public void setUp() {
-        map = new ValueMap("text=");
+        map = new ValueMap("text=x");
+        tester = new BasePageTester(demoApplication);
         tester.startPanel(new TestPanelSource() {
             private static final long serialVersionUID = 1L;
             
@@ -85,6 +89,74 @@ public class UploadPanelTest {
         assertEquals("sample text file for upload testing\n", map.get("fileContents"));
     }
     
+    
+    @Test
+    public void testTextFieldAcceptsInputAfterSubmitIfYouAlsoHaveFile() throws URISyntaxException {
+        FormTester formTester = tester.newFormTester("panel:form");
+        formTester.setValue("text", "aap");
+        formTester.setFile("file", getResourceAsFile("/sample.txt"), "text/plain");
+        formTester.submit();
+        assertEquals("true", map.get("submitSeen"));
+        assertEquals("aap", map.get("text"));
+        assertEquals("aap", map.get("textSeen"));
+    }
+    
+    @Test
+    public void testTextFieldWithoutSetfileIsBroken() throws URISyntaxException {
+        FormTester formTester = tester.newFormTester("panel:form");
+        formTester.setValue("text", "aap");
+        //
+        // Without setFile there is no file in the request,
+        // but it's still multipart.  This causes wicket to ignore the text fields.
+        // With real browsers, that does not happen, since they send a file
+        // with no name and empty content rather than omitting the field altogether
+        // from the reply.  Like so:
+        //
+        //    -----------------------------193884167618819539251897464716
+        //    Content-Disposition: form-data; name="file"; filename=""
+        //    Content-Type: application/octet-stream
+        //
+        //
+        //    -----------------------------193884167618819539251897464716
+        //    Content-Disposition: form-data; name="text"
+        //
+        //    aa
+        //    -----------------------------193884167618819539251897464716--
+        //
+        // Thus, we need a way to add empty file to the request.
+        //
+        formTester.submit();
+        assertEquals("true", map.get("submitSeen"));
+        assertEquals(null, map.get("text"));
+    }
+
+    @Test
+    public void testTextFieldIsAlsoBrokenWithEmptyFileField() throws URISyntaxException {
+        FormTester formTester = tester.newFormTester("panel:form");
+        formTester.setValue("text", "aap");
+        // try simply setting an empty field
+        formTester.setValue("file", "");
+        formTester.submit();
+        assertEquals("true", map.get("submitSeen"));
+        // good: it does not show as file upload
+        assertEquals(false, map.get("haveUpload"));
+        // bad: but it still breaks other text fields.
+        assertEquals(null, map.get("text"));
+    }
+    
+    @Test
+    public void testTextFieldWithFileThatHasNoContentAndNoName() throws URISyntaxException {
+        FormTester formTester = tester.newFormTester("panel:form");
+        formTester.setValue("text", "aap");
+        formTester.setFile("file", getEmptyFile(), "application/octet-stream");
+        formTester.submit();
+        assertEquals("true", map.get("submitSeen"));
+        // good: it does not show as file upload
+        assertEquals(false, map.get("haveUpload"));
+        // also good: the text field is no longer broken.
+        assertEquals("aap", map.get("text"));
+    }
+    
     @Test
     public void testThatResourceCanBeProvidedViaTester() throws URISyntaxException {
         FormTester formTester = tester.newFormTester("panel:form");
@@ -92,6 +164,18 @@ public class UploadPanelTest {
         tester.getServletRequest().addFile("file", getResourceAsFile("/sample.txt"), "image/png");
         formTester.submit();
         assertEquals(true, map.get("haveUpload"));
+    }
+    
+    @Test
+    public void testThatSetFileDoesNotAcceptNull() throws URISyntaxException {
+        FormTester formTester = tester.newFormTester("panel:form");
+        try {
+            formTester.setFile("file", null, null);
+            fail("Null resource when setting file went undetected");
+        }
+        catch (IllegalArgumentException e) {
+            // ok
+        }
     }
     
     @Test
@@ -118,5 +202,26 @@ public class UploadPanelTest {
             throw new IllegalArgumentException("No such resource: " + resourceName);
         }
         return new File(new URI(resource.toString()));
+    }
+    
+    /**
+     * An empty file for use in formTester.setFile().
+     * @return
+     * @throws URISyntaxException
+     */
+    private File getEmptyFile() throws URISyntaxException {
+        String resourceName = "/empty.txt";
+        URL resource = getClass().getResource(resourceName );
+        if (resource == null) {
+            throw new IllegalArgumentException("No such resource: " + resourceName);
+        }
+        return new File(new URI(resource.toString())) {
+            private static final long serialVersionUID = 992382817279158469L;
+
+            @Override
+            public String getName() {
+                return "";
+            }
+        };
     }
 }
