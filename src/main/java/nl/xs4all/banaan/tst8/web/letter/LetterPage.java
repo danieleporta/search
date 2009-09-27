bc62ca4af0ce42951174a666dce04e6dcb46abb1
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-09-27 16:05:45 +0200

    simplify letterPanel constructor

--- src/main/java/nl/xs4all/banaan/tst8/web/letter/LetterPage.java
+++ src/main/java/nl/xs4all/banaan/tst8/web/letter/LetterPage.java
@@ -1,9 +1,6 @@
 package nl.xs4all.banaan.tst8.web.letter;
 
-import org.apache.wicket.model.LoadableDetachableModel;
 
-import nl.xs4all.banaan.tst8.domain.Address;
-import nl.xs4all.banaan.tst8.domain.Letter;
 import nl.xs4all.banaan.tst8.service.ServiceException;
 import nl.xs4all.banaan.tst8.web.base.BasePage;
 
@@ -14,20 +11,7 @@ public class LetterPage extends BasePage {
     
     @Override
     public void doInit() throws ServiceException {
-        add(new LetterPanel("letter", new LetterModel()));
-    }
-    
-    private static class LetterModel extends LoadableDetachableModel {
-        private static final long serialVersionUID = 3207586619607652567L;
-
-        @Override
-        protected Object load() {
-            return new Letter (
-                    new Address("twiet", "aap", "noot"),
-                    new Address("mies", "wim", "zus"),
-                    43L);
-        }
-        
+        add(new LetterPanel("letter"));
     }
 
 }
--- src/main/java/nl/xs4all/banaan/tst8/web/letter/LetterPanel.java
+++ src/main/java/nl/xs4all/banaan/tst8/web/letter/LetterPanel.java
@@ -2,6 +2,8 @@ package nl.xs4all.banaan.tst8.web.letter;
 
 
 
+import nl.xs4all.banaan.tst8.domain.Address;
+import nl.xs4all.banaan.tst8.domain.Letter;
 import nl.xs4all.banaan.tst8.web.address.AddressPanel;
 
 import org.apache.wicket.markup.html.basic.Label;
@@ -9,11 +11,16 @@ import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.ComponentPropertyModel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 
 public class LetterPanel extends Panel {
-    private static final long serialVersionUID = 8379326263858269585L;
 
-    /** model must return letter as object */
+    private static final long serialVersionUID = 8379326263858269585L;
+    
+    public LetterPanel(String panelId) {
+        this (panelId, new LetterPanel.LetterModel());
+    }
+    
     public LetterPanel(String panelId, IModel model) {
         super(panelId, new CompoundPropertyModel(model));
         
@@ -22,4 +29,15 @@ public class LetterPanel extends Panel {
         add(new Label("postage"));
     }
 
+    private static class LetterModel extends LoadableDetachableModel {
+        private static final long serialVersionUID = 3207586619607652567L;
+        
+        @Override
+        protected Object load() {
+            return new Letter (
+                    new Address("twiet", "aap", "noot"),
+                    new Address("mies", "wim", "zus"),
+                    43L);
+        }
+    }
 }
