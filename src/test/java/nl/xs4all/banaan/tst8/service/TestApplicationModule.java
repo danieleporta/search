572eef9e69862f459e1d39c176b3744993e6c0e2
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-11-13 19:49:08 +0100

    move baseApplicationModule from test to production

--- src/test/java/nl/xs4all/banaan/tst8/service/TestApplicationModule.java
+++ src/test/java/nl/xs4all/banaan/tst8/service/TestApplicationModule.java
@@ -1,25 +1,15 @@
 package nl.xs4all.banaan.tst8.service;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 import javax.naming.Context;
 
 import nl.xs4all.banaan.tst8.fixtures.InitializedNamingContext;
 import nl.xs4all.banaan.tst8.fixtures.MailSenderFixture;
-import nl.xs4all.banaan.tst8.service.impl.BuildInfoImpl;
-import nl.xs4all.banaan.tst8.service.impl.JndiReaderImpl;
-import nl.xs4all.banaan.tst8.service.impl.NotificatorImpl;
-import nl.xs4all.banaan.tst8.service.impl.PropertyReaderImpl;
-import nl.xs4all.banaan.tst8.service.impl.ServicesImpl;
-import nl.xs4all.banaan.tst8.web.DemoApplication;
 
+import org.junit.Ignore;
 import org.springframework.mail.MailSender;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import com.google.inject.Scopes;
 import com.google.inject.Singleton;
 
 /**
@@ -28,50 +18,20 @@ import com.google.inject.Singleton;
  * @author konijn
  * 
  */
-public class TestApplicationModule extends AbstractModule {
-
+@Ignore
+public class TestApplicationModule extends BaseApplicationModule {
     private static final String DUMMY_BUILD_PROPERTIES = "/dummy-build.properties";
 
-    @Override
-    protected void configure() {
-        bind(DemoApplication.class).in(Scopes.SINGLETON);
-        bind(Services.class).to(ServicesImpl.class).in(Scopes.SINGLETON);
-        bind(JndiReader.class).to(JndiReaderImpl.class).in(Scopes.SINGLETON);
-
-        bind(Notificator.class).to(NotificatorImpl.class);
-        bind(PropertyReader.class).to(PropertyReaderImpl.class);
-        bind(BuildInfo.class).to(BuildInfoImpl.class);
-    }
-    
-    // TODO: limit this to build properties only
-    // TODO: separate test and production
-    @Provides @Singleton Properties provideBuildProperties() {
-        return getPropertiesFromResource(getBuildPropertyResourceName());
-    }
- 
-    // TODO: production version
-    @Provides @Singleton Context provideContext() {
+    @Override @Provides @Singleton public Context provideContext() {
         return new InitializedNamingContext();
     }
     
-    // TODO: production version
-    @Provides @Singleton MailSender provideMailSender() {
+    @Override @Provides @Singleton public MailSender provideMailSender() {
         return new MailSenderFixture();
     }
 
-    /** helper to read properties from named resource */
-    private Properties getPropertiesFromResource(String resourceName) {
-        InputStream stream = this.getClass().getResourceAsStream(resourceName);
-        Properties properties = new Properties();
-        try {
-            properties.load(stream);
-            return properties;
-        } catch (IOException e) {
-            throw new RuntimeException("missing resource" + resourceName, e);
-        }
-    }
-    
-    private String getBuildPropertyResourceName() {
+    // TODO: decide visibility
+    @Override public String getBuildPropertyResourceName() {
         return DUMMY_BUILD_PROPERTIES;
     }
 }
