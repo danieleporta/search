1069e2f65ca1c83a366ebda7d96ad3ca3417f843
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-11-13 23:14:51 +0100

    replace some spring injection with guice

---
+++ src/test/java/nl/xs4all/banaan/tst8/fixtures/InjectedTest.java
@@ -0,0 +1,29 @@
+package nl.xs4all.banaan.tst8.fixtures;
+
+import nl.xs4all.banaan.tst8.service.Services;
+import nl.xs4all.banaan.tst8.web.DemoApplication;
+import nl.xs4all.banaan.tst8.wiring.TestApplicationModule;
+
+import org.junit.Before;
+import org.junit.Ignore;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+@Ignore
+public abstract class InjectedTest {
+    private Injector injector;
+
+    @Before
+    public void startUp() {
+        injector = Guice.createInjector(new TestApplicationModule());
+    }
+    
+    public Services services() {
+        return injector.getInstance(Services.class);
+    }
+    
+    public DemoApplication application() {
+        return injector.getInstance(DemoApplication.class);
+    }
+}
--- src/test/java/nl/xs4all/banaan/tst8/fixtures/SpringJUnitWicketTest.java
+++ src/test/java/nl/xs4all/banaan/tst8/fixtures/SpringJUnitWicketTest.java
@@ -1,32 +1,23 @@
 package nl.xs4all.banaan.tst8.fixtures;
 
-import javax.annotation.Resource;
-
 import nl.xs4all.banaan.tst8.web.DemoApplication;
 
 import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Base class for tests that want a WicketTester
- * that uses a standard Spring-configured application.
+ * that uses a standard injected application.
  * 
  * @author konijn
  *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"/testContext.xml"})
-public abstract class SpringJUnitWicketTest {
-
-    @Resource
+public abstract class SpringJUnitWicketTest extends InjectedTest {
     protected DemoApplication demoApplication;
     protected BasePageTester tester;
 
     @Before
     public void setUp() {
+        demoApplication = application();
         tester = new BasePageTester(demoApplication);
     }
-
 }
