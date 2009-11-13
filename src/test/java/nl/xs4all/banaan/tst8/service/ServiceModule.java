3bf814847aab6c840a990bad8d3d69640e634f77
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-11-13 17:14:16 +0100

    test guice singleton behaviour

--- src/test/java/nl/xs4all/banaan/tst8/service/ServiceModule.java
+++ src/test/java/nl/xs4all/banaan/tst8/service/ServiceModule.java
@@ -9,40 +9,43 @@ import nl.xs4all.banaan.tst8.service.impl.BuildInfoImpl;
 import nl.xs4all.banaan.tst8.service.impl.JndiReaderImpl;
 import nl.xs4all.banaan.tst8.service.impl.NotificatorImpl;
 import nl.xs4all.banaan.tst8.service.impl.PropertyReaderImpl;
+import nl.xs4all.banaan.tst8.service.impl.ServicesImpl;
 
 import org.springframework.mail.MailSender;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 
 /**
  * How to inject a ServicesImpl for test purposes.
+ * 
  * @author konijn
- *
+ * 
  */
 public class ServiceModule extends AbstractModule {
 
     @Override
     protected void configure() {
-         bind(JndiReader.class).to(JndiReaderImpl.class);
-         bind(Notificator.class).to(NotificatorImpl.class);
-         bind(PropertyReader.class).to(PropertyReaderImpl.class);
-         bind(BuildInfo.class).to(BuildInfoImpl.class);
-         
-         // TODO: this is only production, where eg catalina
-         // has mail.jar to provide MessagingException,
-         // which is not available from normal Maven repo.
-         // bind(MailSender.class).to(JavaMailSenderImpl.class);
-         bind(MailSender.class).to(MailSenderFixture.class);
-         
-         // TODO: limit this to build properties only
-         // TODO: migrate to @Provides
-         // TODO: separate test and production
-         bind(Properties.class).toInstance(
-                 getPropertiesFromResource("/dummy-build.properties"));
-         
-         
+        bind(Services.class).to(ServicesImpl.class).in(Scopes.SINGLETON);
+        bind(JndiReader.class).to(JndiReaderImpl.class).in(Scopes.SINGLETON);
+        bind(Notificator.class).to(NotificatorImpl.class);
+        bind(PropertyReader.class).to(PropertyReaderImpl.class);
+        bind(BuildInfo.class).to(BuildInfoImpl.class);
+
+        // TODO: this is only production, where eg catalina
+        // has mail.jar to provide MessagingException,
+        // which is not available from normal Maven repo.
+        // bind(MailSender.class).to(JavaMailSenderImpl.class);
+        bind(MailSender.class).to(MailSenderFixture.class);
+
+        // TODO: limit this to build properties only
+        // TODO: migrate to @Provides
+        // TODO: separate test and production
+        bind(Properties.class).toInstance(
+                getPropertiesFromResource("/dummy-build.properties"));
+
     }
-    
+
     private Properties getPropertiesFromResource(String resourceName) {
         InputStream stream = this.getClass().getResourceAsStream(resourceName);
         Properties properties = new Properties();
--- src/test/java/nl/xs4all/banaan/tst8/service/ServiceModuleTest.java
+++ src/test/java/nl/xs4all/banaan/tst8/service/ServiceModuleTest.java
@@ -1,47 +1,62 @@
 package nl.xs4all.banaan.tst8.service;
 
 
-import nl.xs4all.banaan.tst8.service.impl.ServicesImpl;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.util.Assert;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class ServiceModuleTest {
 
-    @Test
-    public void testCreatingOneInjector() {
-        Guice.createInjector(new ServiceModule());
+    private Injector injector;
+
+    @Before
+    public void startUp() {
+        injector = Guice.createInjector(new ServiceModule());
     }
-    
+   
     @Test
     public void testCreateOneInstance() {
-        Injector injector = Guice.createInjector(new ServiceModule());
-        injector.getInstance(ServicesImpl.class);
+        injector.getInstance(Services.class);
     }
     
     @Test
     public void testCreateOneInstanceAndTestBindings() {
-        Injector injector = Guice.createInjector(new ServiceModule());
-        Services services = injector.getInstance(ServicesImpl.class);
-        Assert.notNull(services.getBuildInfo());
+        Services services = injector.getInstance(Services.class);
+        assertNotNull(services.getBuildInfo());
     }
     
     @Test
     public void testBuildInfoProperlyWiredUp() {
-        Injector injector = Guice.createInjector(new ServiceModule());
-        Services services = injector.getInstance(ServicesImpl.class);
-        Assert.notNull(services.getBuildInfo());
-        Assert.notNull(services.getBuildInfo().getName());
+        Services services = injector.getInstance(Services.class);
+        assertNotNull(services.getBuildInfo());
+        assertNotNull(services.getBuildInfo().getName());
     }
     
     @Test
     public void testNotifactorWiredUp() {
-        Injector injector = Guice.createInjector(new ServiceModule());
-        Services services = injector.getInstance(ServicesImpl.class);
-        Assert.notNull(services.getNotificator());
+        Services services = injector.getInstance(Services.class);
+        assertNotNull(services.getNotificator());
         // cant see inside
     }
+    
+    @Test
+    public void testSingletonStaysUnchangesInsideInjector() {
+        Services services = injector.getInstance(Services.class);
+        Services services2 = injector.getInstance(Services.class);
+        assertTrue(services == services2);
+    }
+    
+    @Test
+    public void testSingletonHasMeaningOnlyInASingleInjector() {
+        Services services = injector.getInstance(Services.class);
+        Injector injector2 = Guice.createInjector(new ServiceModule());
+        Services services2 = injector2.getInstance(Services.class);
+        assertTrue(services != services2);
+    }
+    
 }
