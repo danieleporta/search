eb2e94ff7ca3f0912c388d49f9058da61447bde8
Erik van Konijnenburg <ekonijn@xs4all.nl>
2009-12-12 18:45:42 +0100

    add @ignore to some test infrastructure

--- src/test/java/nl/xs4all/banaan/tst8/wiring/TestModule.java
+++ src/test/java/nl/xs4all/banaan/tst8/wiring/TestModule.java
@@ -1,8 +1,11 @@
 package nl.xs4all.banaan.tst8.wiring;
 
+import org.junit.Ignore;
+
 import com.google.inject.AbstractModule;
 
 /** Guice module containing both shared and testing bindings */
+@Ignore
 public class TestModule extends AbstractModule {
 
     @Override
