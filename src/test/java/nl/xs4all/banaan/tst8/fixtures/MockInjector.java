3859a4fb1cc6d617ccf9c73cd181ec086db4b7d2
Erik van Konijnenburg <ekonijn@xs4all.nl>
2010-01-08 21:01:22 +0100

    do the mock injector based on keys

--- src/test/java/nl/xs4all/banaan/tst8/fixtures/MockInjector.java
+++ src/test/java/nl/xs4all/banaan/tst8/fixtures/MockInjector.java
@@ -1,47 +1,60 @@
 package nl.xs4all.banaan.tst8.fixtures;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import nl.xs4all.banaan.tst8.wiring.TestModule;
 
 import org.easymock.EasyMock;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Key;
 import com.google.inject.util.Modules;
 
 
 /**
  * Wrapper for a normal test injector, except that
- * specific interfaces can be mocked.
+ * specific interfaces or keys can be mocked.
  * @author konijn
  *
  */
 public class MockInjector {
     protected final Injector injector;
-    private final Class<?>[] mockedClasses;
+    private final List<Key<?>> mockedKeys;
     
     public MockInjector(Class<?>... mockedClasses) {
-        this.mockedClasses = mockedClasses;
+        this.mockedKeys = new ArrayList<Key<?>>();
+        for (Class<?> clazz : mockedClasses) {
+            mockedKeys.add(Key.get(clazz));
+        }
         injector = Guice.createInjector(
                 Modules.override(new TestModule()).
-                        with(new MockModule(mockedClasses)));
+                with(new MockModule(this.mockedKeys)));
     }
     
     /** retrieve object from the underlying injector */
     public <T> T get(Class<T> clazz) {
         return injector.getInstance(clazz);
     }
+    
+    /** retrieve object from underlying injector */
+    public <T> T get(Key<T> key) {
+        return injector.getInstance(key);
+    }
 
     /** put all mock objects in replay mode */
     public void replay() {
-        for (Class<?> c: mockedClasses) {
-            EasyMock.replay(get(c));
+        for (Key<?> key: mockedKeys) {
+            EasyMock.replay(get(key));
         }
     }
 
     /** verify all mock objects have seen the expected invocations */
     public void verify() {
-        for (Class<?> c: mockedClasses) {
-            EasyMock.verify(get(c));
+        for (Key<?> key: mockedKeys) {
+            EasyMock.verify(get(key));
         }
     }
 }
--- src/test/java/nl/xs4all/banaan/tst8/fixtures/MockModule.java
+++ src/test/java/nl/xs4all/banaan/tst8/fixtures/MockModule.java
@@ -2,25 +2,33 @@ package nl.xs4all.banaan.tst8.fixtures;
 
 import static org.easymock.EasyMock.createMock;
 
+import java.util.List;
+
 import com.google.inject.AbstractModule;
+import com.google.inject.Key;
 import com.google.inject.binder.AnnotatedBindingBuilder;
 
 /**
- *  Guice config that provides mocks for classes indicated in constructor;
+ *  Guice config that provides mocks for keys indicated in constructor;
  *  use in combination with {@link MockInjector} 
  */
 public class MockModule extends AbstractModule {
-    private final Class<?>[] mockedClasses;
+    private final List<Key<?>> mockedKeys;
 
-    public MockModule(Class<?>[] mockedClasses) {
+    public MockModule(List<Key<?>> mockedKeys) {
         super();
-        this.mockedClasses = mockedClasses;
+        this.mockedKeys = mockedKeys;
     }
     
     @Override @SuppressWarnings("unchecked")
     protected void configure() {
-        for (Class<?> c : mockedClasses) {
-            ((AnnotatedBindingBuilder<Object>) bind(c)).toInstance(createMock(c));
+        for (Key<?> key : mockedKeys) {
+            ((AnnotatedBindingBuilder<Object>) bind(key)).toInstance(createMock(interfaceToMock(key)));
         }
+    }
+
+    /** the interface to be mocked for this key */
+    public Class<?> interfaceToMock(Key<?> key) {
+        return key.getTypeLiteral().getRawType();
     }
 }
