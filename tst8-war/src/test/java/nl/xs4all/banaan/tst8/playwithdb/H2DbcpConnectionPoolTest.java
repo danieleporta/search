8da1974e7d1ed942bb8f1e7878f93dd4069208cb
Erik van Konijnenburg <ekonijn@xs4all.nl>
2010-05-22 23:18:02 +0200

    add dbcp example.

---
+++ tst8-war/src/test/java/nl/xs4all/banaan/tst8/playwithdb/H2DbcpConnectionPoolTest.java
@@ -0,0 +1,93 @@
+package nl.xs4all.banaan.tst8.playwithdb;
+
+
+import java.sql.Connection;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import junit.framework.Assert;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp.BasicDataSourceFactory;
+import org.junit.Test;
+
+/**
+ * Providing H2 database connection via a DBCP connection pool.
+ * This pool is not tied to a particular type of database. 
+ */
+public class H2DbcpConnectionPoolTest {
+
+    @Test
+    public void testDataSourceIsLimited() {
+        DataSource source = makeDataSource();
+        int i = 0;
+        try {
+            while (true) {
+                source.getConnection();
+                i++;
+            }
+        }
+        catch (Exception e) {
+            Assert.assertEquals(8, i);
+        }
+    }
+
+    private DataSource makeDataSource() {
+        // In eg Jetty, the DataSource is created directly and added to JNDI
+        BasicDataSource source = new BasicDataSource();
+        source.setDriverClassName("org.h2.Driver");
+        source.setUrl("jdbc:h2:mem:mydb");
+        // default unlimited
+        source.setMaxOpenPreparedStatements(10);
+        // default 8
+        source.setMaxActive(8);
+        // default 8
+        source.setMaxIdle(8);
+        source.setUsername("");
+        source.setPassword("");
+        source.setDefaultTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+        // setLoginTimeout not supported.
+        // wait this in msec when trying to get connection.  <= 0 is forever.
+        source.setMaxWait(1);
+        return source;
+    }
+
+    @Test
+    public void testDataSourceFromFactoryIsLimited() throws Exception {
+        DataSource source = makeDataSourceViaFactory();
+        int i = 0;
+        try {
+            while (true) {
+                source.getConnection();
+                i++;
+            }
+        }
+        catch (Exception e) {
+            Assert.assertEquals(8, i);
+        }
+    }
+
+    private DataSource makeDataSourceViaFactory() throws Exception {
+        // In Tomcat, the datasource is created via a factory, 
+        // fed by properties stored next to the factory in JNDI.
+        // the actual construction is then done with
+        // factory.getObjectInstance(obj, name, nameCtx, environment);
+        // see http://tomcat.apache.org/tomcat-4.1-doc/printer/jndi-datasource-examples-howto.html
+        
+        Properties properties = new Properties();
+        properties.put("driverClassName", "org.h2.Driver");
+        properties.put("url", "jdbc:h2:mem:mydb");
+        properties.put("username", "");
+        properties.put("password", "");
+        properties.put("maxOpenPreparedStatements", "10");
+        properties.put("maxActive", "8");
+        properties.put("maxIdle", "8");
+        properties.put("defaultTransactionIsolation", "2"); // read commited        
+        properties.put("maxWait", "1");
+        // setLoginTimeout not supported.
+        // wait this in msec when trying to get connection.  <= 0 is forever.
+        return BasicDataSourceFactory.createDataSource(properties);
+    }
+
+}
